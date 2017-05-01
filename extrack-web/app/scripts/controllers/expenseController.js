d58f9aa2480158875a8c1454e906c99ca78d30f2
joshisayali <sayali.j23@gmail.com>
2017-04-30 17:34:59 -0700

    Code added to create new expense

--- extrack-web/app/scripts/controllers/expenseController.js
+++ extrack-web/app/scripts/controllers/expenseController.js
@@ -2,7 +2,8 @@
 angular.module('extrackWebApp')
 .controller('ExpenseController', ['$scope','expenseFactory', function($scope, expenseFactory){
     $scope.message = 'this is expense controller';
-      
+    $scope.newExpense = {expenseDate:'',expenseItem:'',expenseAmount:'',expensePayment:'',expenseSubCategory:'',expenseRepeat:''};
+    
     $scope.expenses = expenseFactory.getExpenses().query(
         function(response){
             $scope.expenses = response;
@@ -11,6 +12,43 @@ angular.module('extrackWebApp')
             console.log('Error: '+error);
         }
     );
+    
+    $scope.expenseCategories = expenseFactory.getExpenseCategories().query(
+        function(response){
+            $scope.expenseCategories = response;
+        },function(error){
+            console.log('Error: '+error);
+        });
+    
+    $scope.expenseSubCategories = expenseFactory.getExpenseSubCategories().query(
+        function(response){
+            $scope.expenseSubCategories = response;
+        },function(error){
+            console.log('Error: '+error);
+        });
+    
+    $scope.expensePayments = expenseFactory.getExpensePayments().query(
+        function(response){
+            $scope.expensePayments = response;
+        },function(error){
+            console.log('Error: '+error);
+        });
+    
+    $scope.expenseRepeats = expenseFactory.getExpenseRepeats().query(
+        function(response){
+            $scope.expenseRepeats = response;
+        },function(error){
+            console.log('Error: '+error);
+        });
+    
+    $scope.createExpense = function(){
+        
+        console.log($scope.newExpense);
+        expenseFactory.getExpenses().create($scope.newExpense);
+        $scope.newExpense = {expenseDate:'',expenseItem:'',expenseAmount:'',expensePayment:'',expenseSubCategory:'',expenseRepeat:''};
+        $scope.createExpenseForm.$setPristine();
+    };
+    
     //display total amount
     $scope.total = 0;
     for(var i=0; i < $scope.expenses.length; i++)
--- extrack-web/app/scripts/services/expenseService.js
+++ extrack-web/app/scripts/services/expenseService.js
@@ -3,9 +3,25 @@ angular.module('extrackWebApp')
 .constant('baseURL','http://localhost:3000/')
 .service('expenseFactory',['$resource','baseURL',function($resource,baseURL){
     
-     this.getExpenses = function(){
-        return $resource(baseURL+'expenses');   
+    this.getExpenses = function(){
+        return $resource(baseURL+'expenses',null,{create:{method:'POST'}});   
     };  
+    
+    this.getExpenseCategories = function(){
+        return $resource(baseURL+'expensecategories');
+    };
+    
+    this.getExpenseSubCategories = function(){
+        return $resource(baseURL+'expensesubcategories');
+    };
+    
+    this.getExpensePayments = function(){
+        return $resource(baseURL+'expensepayments');
+    };
+    
+    this.getExpenseRepeats = function(){
+        return $resource(baseURL+'expenserepeats');
+    };
     
     
 }]);
